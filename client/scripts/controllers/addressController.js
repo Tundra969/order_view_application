4a1db1d829aaf1dcee1e7625ff4781c38e9d0935
Dana Hazen <danahazen79@gmail.com>
2015-11-16 14:38:58 -0600

    initial commit

---
+++ client/scripts/controllers/addressController.js
@@ -0,0 +1,4 @@
+myApp.controller('addressController', ['$scope', function($scope){
+    console.log("Address Controller");
+    $scope.thing = "Addresses here";
+}]);
---
+++ client/scripts/controllers/orderController.js
@@ -0,0 +1,4 @@
+myApp.controller('orderController', ['$scope', function($scope){
+    console.log("Order Controller");
+    $scope.thing = "Orders here";
+}]);
